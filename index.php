cade1515e9424c4488502664a224b4189396e5a4
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-11-21 00:30:28 +0100

    admin redirection

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +16,21 @@ include 'db.php';
      <p>
         <a href="register.php">Register</a>
     </p>
+
+<?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true):?>
     <p>
+        <a href="admin.php">Admin</a>
+    </p>
+     <p>
+        <a href="logout.php">Logout</a>
+    </p>
+    <?php else: ?>
+         <p>
         <a href="login.php">Login</a>
     </p>
+
+<?php endif; ?>
+
+   
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 
 <?php
 include 'db.php';
+session_start();
+
+if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true){
+    header("Location: admin.php");
+    exit;
+}
 
 $error = '';
 
